5a17438475a4353822316946325117d07fdc3e85
IgnaDev <arcortoon@gmail.com>
2024-01-14 21:10:50 -0300

    feat: implementa la capacidad de paso de parámetros en las rutas
    Descripción:
    Se completó la tarea 6, que consistía en integrar la capacidad de paso de parámetros a través de las rutas en nuestra aplicación React utilizando la versión 6 de react-router-dom.
    
    Detalles de la implementación:
    - Se configuró el sistema de enrutamiento para permitir la pasada de parámetros a través de las rutas.
    - Se actualizaron las rutas y componentes necesarios para admitir la recepción y procesamiento de parámetros.
    
    Próximos pasos:
    Ahora, se procederá a explorar y mejorar la configuración de pruebas en el proyecto, ya que el testing ha sido realizado de manera limitada hasta el momento. Se busca comprender y optimizar el proceso de testing para garantizar la robustez y confiabilidad del código.
    
    Criterios de aceptación cumplidos:
    - Se implementó la capacidad de paso de parámetros a través de las rutas.
    
    Nota para el equipo:
    En la siguiente fase, se enfocará en mejorar la cobertura de pruebas y comprender mejor el proceso de testing en el proyecto.

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Alquilar from '../pages/Alquilar';
+import DetalleAlquiler from '../pages/DetalleAlquiler';
 import Usuario from '../pages/Usuario'; 
 import PrivateRoute from './PrivateRoute';
 import NotFound from '../pages/NotFound';
@@ -10,9 +11,9 @@ export default function Router() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/alquilar" element={<Alquilar />} />
+      <Route path="/alquilar/:id" element={<DetalleAlquiler />} />
       <Route path="/user" element={<PrivateRoute element={<Usuario />} />} />
       <Route path="*" element={<NotFound />} /> 
     </Routes>
   );
 }
-
