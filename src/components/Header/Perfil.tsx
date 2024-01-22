41fdbb6dfe9e808bea7d0f357ad3b1ec5d6ae6d6
IgnaDev <arcortoon@gmail.com>
2024-01-22 17:16:01 -0300

    feat(header): Completa la versión móvil del Header con iconos de la lista
    Se ha finalizado la implementación del Header para dispositivos móviles, incluyendo la adición de cada uno de los iconos necesarios en la lista a mostrar.

--- src/components/Header/Perfil.tsx
+++ src/components/Header/Perfil.tsx
@@ -1,7 +1,7 @@
 interface PerfilProps {
-    perfilIMG?: string;
-    PerfilName: string;
-    open: boolean
+  perfilIMG?: string;
+  PerfilName: string;
+  open: boolean;
 }
 
 export default function Perfil ({ perfilIMG = 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-Photo.png', PerfilName, open }: PerfilProps) {
@@ -10,7 +10,7 @@ export default function Perfil ({ perfilIMG = 'https://www.pngall.com/wp-content
 
   return (
     <>
-      <div className={`${!open && 'hidden transition-none'} flex sm:mt-6 mt-4 gap-4`}>
+      <div className={`flex sm:mt-6 mt-4 gap-4 transition-all duration-300 ${open ? 'opacity-100 visible delay-150' : 'opacity-0 invisible delay-0'}`}>
         <div>
           <img className='rounded-full object-cover w-20 h-20 ' src={perfilIMG} alt='' />
         </div>
