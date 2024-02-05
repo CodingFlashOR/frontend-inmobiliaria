328d510d8926dc5c0f39aea5d2f7e81a8ef320b3
IgnaDev <arcortoon@gmail.com>
2024-02-05 18:14:15 -0300

    feat: Completa maquetado del MainBanner para la página de inicio
    Se ha finalizado el maquetado del MainBanner para la página de inicio, incluyendo el diseño y la estructura proporcionados en Figma.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { AlquilarIcon, ComprarIcon, ContactoIcon, HomeIcon, FacebookIcon, Whatsa
 export default function Footer () {
   return (
     <footer className='fixed bottom-0 left-0 w-full'>
-      <div className='container mx-auto px-4 flex justify-between items-center gap-1 md:w-max-2/3 lg:hidden'>
+      <div className='container mx-auto px-4 flex justify-between items-center gap-1 md:w-max-2/3 lg:hidden bg-white-200 backdrop-blur-lg'>
         <Icono
           titulo='Home'
           icono={HomeIcon}
--- src/components/Home/Carousel/Carousel.tsx
+++ src/components/Home/Carousel/Carousel.tsx
@@ -7,32 +7,48 @@ export default function Carousel () {
   const listRef = useRef<HTMLUListElement>(null)
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  const scrollStep = (now: number, listNode: HTMLUListElement, startScroll: number, scrollLeft: number, scrollDuration: number, startTime: number) => {
+    const elapsed = now - startTime
+    const t = Math.min(elapsed / scrollDuration, 1)
+    listNode.scrollLeft = startScroll + (scrollLeft - startScroll) * t
+    if (t < 1) {
+      window.requestAnimationFrame((now) => scrollStep(now, listNode, startScroll, scrollLeft, scrollDuration, startTime))
+    }
+  }
+
   useEffect(() => {
     const listNode = listRef.current
     const imgNode = listNode?.querySelectorAll('li > div > img')[currentIndex]
 
-    if (imgNode) {
-      imgNode.scrollIntoView({
-        behavior: 'smooth'
-      })
+    if (imgNode && listNode) {
+      const imgElement = imgNode as HTMLImageElement
+      const scrollLeft = imgElement.offsetLeft - (listNode.offsetWidth / 2) + (imgElement.offsetWidth / 2)
+
+      const scrollDuration = 500
+      const startTime = performance.now()
+      const startScroll = listNode.scrollLeft
+
+      window.requestAnimationFrame((now) => scrollStep(now, listNode, startScroll, scrollLeft, scrollDuration, startTime))
     }
-
-    const timer = setTimeout(() => {
-      setCurrentIndex((currentIndex + 1) % data.length)
-    }, 3000)
-
-    return () => clearTimeout(timer)
   }, [currentIndex])
 
-  const goToSlide = (indice : number) => {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length)
+    }, 6000)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
+  const goToSlide = (indice: number) => {
     setCurrentIndex(indice)
   }
 
   const scrollToImage = (dir: string) => {
     if (dir === 'prev') {
-      setCurrentIndex(curr => {
-        const isFirsSlide = currentIndex === 0
-        return isFirsSlide ? 0 : curr - 1
+      setCurrentIndex((curr) => {
+        const isFirstSlide = currentIndex === 0
+        return isFirstSlide ? 0 : curr - 1
       })
     } else {
       const isLastSlide = currentIndex === data.length - 1
@@ -43,46 +59,47 @@ export default function Carousel () {
   }
 
   return (
-    <div className='h-20 lg:h-40 w-full sm:w-96 md:w-3/4 m-auto sm:mt-5 main-container'>
+    <div className='h-20 lg:h-40 w-full sm:w-4/5 md:w-3/4 lg:w-full m-auto main-container mt-2 sm:mt-5'>
       <div className='slider-container'>
         <div
           className='leftArrow text-sm text-amarillo-oscuro'
           onClick={() => scrollToImage('prev')}
-        >❬
+        >
+          ❬
         </div>
         <div
           className='rightArrow text-sm text-amarillo-oscuro'
           onClick={() => scrollToImage('next')}
-        >❭
+        >
+          ❭
         </div>
-        <div className='container-images '>
+        <div className='container-images'>
           <ul ref={listRef} className=''>
-            {
-              data.map((item: DataType) => {
-                return (
-                  <li key={item.id}>
-
-                    <div className=''>
-                      <img src={item.imgUrl} className='imagen' />
-                    </div>
-
-                  </li>
-                )
-              })
-            }
+            {data.map((item: DataType) => {
+              return (
+                <li key={item.id}>
+                  <div aria-hidden='true'>
+                    <img src={item.imgUrl} className='imagen' />
+                  </div>
+                </li>
+              )
+            })}
           </ul>
         </div>
         <div className='dots-container'>
-          {
-            data.map((_, idx) => (
-              <div
-                key={idx} className={`dot-container-item text-amarillo ${idx === currentIndex ? 'active text-amarillo-oscuro' : ''}`}
-                onClick={() => goToSlide(idx)}
-              >
-                &#9865;
-              </div>
-            ))
-          }
+          {data.map((_, idx) => (
+            <div
+              key={idx}
+              className={`dot-container-item text-amarillo ${
+                idx === currentIndex ? 'active text-amarillo-oscuro' : ''
+              }`}
+              onClick={() => goToSlide(idx)}
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
+            >
+              ⚉
+            </div>
+          ))}
         </div>
       </div>
     </div>
