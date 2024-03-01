f208177c4a3409a237e32683ff77ff9980893959
IgnaDev <arcortoon@gmail.com>
2024-03-01 14:55:06 -0300

    fix/feat: Corrige error en el Carousel y comienza trabajo en la página de alquiler y la card
    Se ha corregido un error en el componente Carousel que impedía su funcionamiento adecuado. Además, se ha comenzado el trabajo en la implementación de la página de alquiler y la card para las propiedades.

--- src/pages/Alquilar.tsx
+++ src/pages/Alquilar.tsx
@@ -1,10 +1,12 @@
 import Layout from '../layouts/Layout'
+import Carousel from '../components/Carousel/Carousel'
+import CardInmueble from '../components/CardInmueble/CardInmueble'
 
 export default function Alquilar () {
   return (
     <Layout>
-      <div>Alquilar</div>
-
+      <Carousel />
+      <CardInmueble />
     </Layout>
   )
 }
