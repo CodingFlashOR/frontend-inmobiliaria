05520dc55645bab0b81b7b57966eee286aff4680
IgnaDev <arcortoon@gmail.com>
2024-03-27 09:27:56 -0300

    chore: Eliminar Tailwind del proyecto

--- src/components/Footer/Icono.tsx
+++ src/components/Footer/Icono.tsx
@@ -3,10 +3,10 @@ interface IconoProps {
   titulo: string;
 }
 
-const Icono: React.FC<IconoProps> = ({ icono: IconoComponent, titulo }) => {
+const Icono: React.FC<IconoProps> = ({ icono: IconComponent, titulo }) => {
   return (
     <div className='flex flex-col items-center justify-center mt-2'>
-      <IconoComponent />
+      <IconComponent />
       <p className=' text-amarillo text-sm'>{titulo}</p>
     </div>
   )
